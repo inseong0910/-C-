8326081ca7ec503abcec3035055dab07500ba97f
Hwang Inseong <88603039+inseong0910@users.noreply.github.com>
2024-09-14 11:19:15 +0900

    Add files via upload

---
+++ chapter2_240914.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("Be happy");
+	printf("My friend");
+	return 0;
+}
+
+
+#include <stdio.h>
+int main(void)
+{
+	printf("Be happy\n");
+	printf("1234567890\n");
+	printf("My\friend\n");
+	printf("Goot\bd\tchance\n");
+	printf("Cow\rW\a\n");
+	return 0;
+}
+
+
+#include <stdio.h>
+int main(void)
+{
+	printf("%d\n", 10);
+	printf("%lf\n", 3.4);
+	printf("%.1lf\n", 3.45);
+	printf("%.10lf\n", 3.4); //소수점 이하 10자리까지 출력
+
+	return 0;
+}
